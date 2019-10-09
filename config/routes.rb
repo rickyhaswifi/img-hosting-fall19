2a408db13e3af239d6b550701803ab1752a376d4
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-10-08 19:20:18 -0600

    add procfile

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     put '/users/:id', to: 'api/users#update'
   end 
+
+  get '*other', to: 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
